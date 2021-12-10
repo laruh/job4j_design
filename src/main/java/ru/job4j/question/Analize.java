f037e6162f7aa563f12e5941119f43ac9f1f2545
laruh <oblomoff616@gmail.com>
2021-12-10 17:48:04 +0700

    2. Статистика по коллекции. [#45889]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -1,6 +1,7 @@
 package ru.job4j.question;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -10,22 +11,20 @@ public class Analize {
         Info rsl = new Info(0, 0, 0);
         Map<Integer, String> curMap = current.stream()
                 .collect(Collectors.toMap(User::getId, User::getName));
-        int add = 0;
         int changed = 0;
         int deleted = 0;
         for (User user : previous) {
             String nameCurrent = curMap.get(user.getId());
-            if (nameCurrent != null && !curMap.get(user.getId()).equals(user.getName())) {
+            if (nameCurrent != null && !Objects.equals(nameCurrent, user.getName())) {
                 changed += 1;
                 curMap.remove(user.getId());
-            } else if (nameCurrent != null && curMap.get(user.getId()).equals(user.getName())) {
+            } else if (nameCurrent != null && Objects.equals(nameCurrent, user.getName())) {
                 curMap.remove(user.getId());
             } else if (nameCurrent == null) {
                 deleted += 1;
             }
         }
-        add += curMap.size();
-        rsl.setAdded(add);
+        rsl.setAdded(curMap.size());
         rsl.setChanged(changed);
         rsl.setDeleted(deleted);
         return rsl;
