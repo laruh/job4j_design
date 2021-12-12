e0ea7769edad4b78ca449bcd4307184cbbf64f3e
laruh <oblomoff616@gmail.com>
2021-12-12 20:31:00 +0700

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,61 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public Map<String, String> getValues() {
+        return values;
+    }
+
+    public void load() {
+        try (BufferedReader in = new BufferedReader(new FileReader(this.path))) {
+            in.lines()
+                    .filter(line -> !Objects.equals(line, null)
+                            && !line.contains("# "))
+                    .forEach(line -> {
+                        String[] elem = line.split("=");
+                        if (elem.length == 2) {
+                            values.put(elem[0], elem[1]);
+                        }
+                    });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        if (values.get(key) == null) {
+            throw new UnsupportedOperationException("Don't impl this method yet!");
+        }
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        System.out.println(config.getValues());
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertNull;
+
+public class ConfigTest {
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"), is("Petr Arsentev"));
+        assertNull(config.value("surname"));
+    }
+
+    @Test
+    public void whenPair() {
+        String path = "./data/app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.dialect"),
+                is("org.hibernate.dialect.PostgreSQLDialect"));
+        assertThat(config.value("hibernate.connection.url"),
+                is("jdbc:postgresql://127.0.0.1:5432/trackstudio"));
+    }
+
+    @Test
+    public void whenPairWithNoSpaces() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.getValues().size(), is(2));
+    }
+}
