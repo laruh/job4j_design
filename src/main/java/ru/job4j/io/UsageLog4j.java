2c39f630d3c725b3474cc0be60f397dba189d74d
laruh <oblomoff616@gmail.com>
2021-12-27 20:51:15 +0700

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,20 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Petr Arsentev";
+        int age = 33;
+        String fileTrace = "This is trace file";
+        String pathTrace = "./project/trace.txt";
+        String info = "Print log";
+        int number = 2;
+        String warning = "Dont delete file";
+        String fileWarning = "txt.txt";
+        String error = "Connection refused to host";
+        String errorName = "https://job4j.ru/";
+        LOG.trace("Trace info: {}, {}", fileTrace, pathTrace);
+        LOG.debug("User info name : {}, age : {}", name, age);
+        LOG.info("You need to: {} {} times", info, number);
+        LOG.warn("Warning! {}: {}", warning, fileWarning);
+        LOG.error("Error! {}: {}", error, errorName);
     }
 }
